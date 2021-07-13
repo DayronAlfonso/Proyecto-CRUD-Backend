df00a605bfba26ac27344a59d57fa53d3ceb9dff
Dayron Alfonso <daironalfonso15@gmail.com>
2021-07-13 17:18:13 -0500

    Se configuran las dependencias y se inicializa el Spring Boot

---
+++ src/main/java/com/djaa/ProyectoCRUDBackend/ProyectoCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.djaa.ProyectoCRUDBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCrudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djaa/ProyectoCRUDBackend/ProyectoCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djaa.ProyectoCRUDBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
