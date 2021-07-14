4ba6b3b52112c586c4d0c712f8c29f96ca2f76c5
Dayron Alfonso <daironalfonso15@gmail.com>
2021-07-14 10:36:39 -0500

    Se crea el repositorio de PaisRepository.java

---
+++ src/main/java/com/djaa/ProyectoCRUDBackend/repository/PaisRepository.java
@@ -0,0 +1,9 @@
+package com.djaa.ProyectoCRUDBackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.djaa.ProyectoCRUDBackend.model.Pais;
+
+public interface PaisRepository extends JpaRepository<Pais, Long>{
+
+}
