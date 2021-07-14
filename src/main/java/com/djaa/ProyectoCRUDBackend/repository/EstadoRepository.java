0a618345dc6a9a249c5f82d89a390afd5725c80a
Dayron Alfonso <daironalfonso15@gmail.com>
2021-07-14 10:37:54 -0500

    Se crea el repositorio de EstadoRepository.java

---
+++ src/main/java/com/djaa/ProyectoCRUDBackend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.djaa.ProyectoCRUDBackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.djaa.ProyectoCRUDBackend.model.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+
+}
