e0788e21ad83f76a5dbd69373e851501d6123e28
Dayron Alfonso <daironalfonso15@gmail.com>
2021-07-22 22:22:56 -0500

    Se crea el Rest de obtener el pais por el id

--- src/main/java/com/djaa/ProyectoCRUDBackend/rest/PaisRest.java
+++ src/main/java/com/djaa/ProyectoCRUDBackend/rest/PaisRest.java
@@ -3,9 +3,11 @@ package com.djaa.ProyectoCRUDBackend.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
 
 import com.djaa.ProyectoCRUDBackend.model.Pais;
 import com.djaa.ProyectoCRUDBackend.service.PaisService;
@@ -22,7 +24,11 @@ public class PaisRest {
 	private ResponseEntity<List<Pais>> getAllPais(){
 		return ResponseEntity.ok(paisService.findAll());
 	}
-	
+	@GetMapping ("{id}")
+	private ResponseEntity<Optional<Pais>> getAllByPais(@PathVariable Long id){
+		Optional<Pais> queryPais = paisService.findById(id);
+		return ResponseEntity.ok(queryPais);
+	}
 	
 
 }
